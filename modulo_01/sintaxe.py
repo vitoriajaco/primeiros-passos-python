2f4b7ce01e9b1d1b01714ffdb6f2eb260033da7b
vitoriajaco <vitoriajaco@gmail.com>
2024-11-09 18:35:08 -0300

    Comentarios e indentação

---
+++ modulo_01/sintaxe.py
@@ -0,0 +1,19 @@
+# Para fazer comentario de uma linha 
+
+""" Para fazer comentarios que pulam linhas
+
+.... basta usar 3 aspas duplas """
+
+nome = "Vitoria " + \
+"Jaco" #Serve para pular linha de maneira mais organizada
+
+print(nome)
+
+#BLOCOS 
+
+idade = 25
+
+if idade < 18:
+    print ("Menor de idade")
+else: 
+    print("Maior de idade")    
