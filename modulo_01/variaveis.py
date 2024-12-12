9523c174b4a65626f91b2cdf98f9aee665cd6c6b
Vitória Jacó <vitoriajaco@192.168.1.11>
2024-12-12 15:46:52 -0300

    variaveis no py e como concatenar string + int

---
+++ modulo_01/variaveis.py
@@ -0,0 +1,10 @@
+nome_completo = "Maria Clementina"
+
+idade = 24
+
+print(nome_completo)
+
+print("Meu nome é " + nome_completo + " e minha idade é " + str(idade))
+
+#Como idade é um número inteiro (int), ele precisa ser convertido para string usando str() 
+# antes de ser concatenado com outras strings.
