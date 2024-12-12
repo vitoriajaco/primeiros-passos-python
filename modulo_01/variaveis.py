9befc0ded795bac2a421502dc8a9e174359c9bde
Vitória Jacó <vitoriajaco@192.168.1.11>
2024-12-12 16:18:52 -0300

    numeros e tipos em py

--- modulo_01/variaveis.py
+++ modulo_01/variaveis.py
@@ -8,3 +8,18 @@ print("Meu nome é " + nome_completo + " e minha idade é " + str(idade))
 
 #Como idade é um número inteiro (int), ele precisa ser convertido para string usando str() 
 # antes de ser concatenado com outras strings.
+
+#Inteiro
+
+numero_inteiro = 3
+
+#Real com ponto flutuante
+
+numero_real = 3.14
+
+print ("soma = ", numero_inteiro + numero_real)
+print(numero_real, numero_inteiro)
+
+#type 
+
+print("O tipo da variavel é: ", type(numero_real))
