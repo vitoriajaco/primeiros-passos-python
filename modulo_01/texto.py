bb45ccc7f4e0f8299151df96750c14c890c26b69
vitoriajaco <vitoriajaco@gmail.com>
2024-12-13 22:57:10 -0300

    formas de inserir string

---
+++ modulo_01/texto.py
@@ -0,0 +1,17 @@
+#Declaracao
+
+nome_completo = "Maria Joaquina do Amaral Pereira Goes"
+
+
+#Dessa forma da pra quebrar em linhas
+nome_com_aspas = """Maria 
+
+Elismando
+
+da 
+
+Silva"""
+
+print(nome_completo)
+
+print(nome_com_aspas)
