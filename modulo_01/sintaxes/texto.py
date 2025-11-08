7019257286ead44b4ea1e8ceb9649bcc68b479ef
vitoriajaco <vitoriajaco@gmail.com>
2025-11-08 16:21:34 -0300

    tratamento de dados em python .replace e .find

--- modulo_01/sintaxes/texto.py
+++ modulo_01/sintaxes/texto.py
@@ -19,6 +19,18 @@ print(nome_com_aspas)
 nome = "Lupércio"
 sobrenome = "Santos"
 
+#Se a letra não estiver presente ele retornará -1
+#o find mostra em que posicao está do index
+a = nome.find("i")
+print(a)
+
+#Para substituir uma letra por outra
+sobrenome = sobrenome.replace("o", "e")
+print(sobrenome)
+
+telefone = "(87)99991534"
+telefone = telefone.replace("(","",).replace(")","",)
+print(telefone)
 #Formatacao
 
 print("Meu nome é", "Maria", "Sebastiana")
